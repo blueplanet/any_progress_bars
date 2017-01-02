0194a74f86229213464e83bc1b5dd8b43ea2ef78
blueplanet <erguolinge@gmail.com>
2017-01-02 09:35:54 +0900

    一覧デザイン調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :progress_bars, only: %i(new create index)
+  resources :progress_bars
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
