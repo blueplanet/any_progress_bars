b7155073cc38b7aac8ee8b65235126f25a75dbaf
blueplanet <erguolinge@gmail.com>
2017-01-03 08:17:38 +0900

    リファクタリング

--- app/models/progress_bar.rb
+++ app/models/progress_bar.rb
@@ -5,7 +5,17 @@ class ProgressBar < ApplicationRecord
   validates :total, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :current, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  scope :notify_for(time), -> {
+    where.not(slack_url: nil).where(notify_time: time.to_s(:time))
+  }
+
   def current_text
     (current * 100 / total).to_i
+  end
+
+  def send_slack_notify
+    notifier = Slack::Notifier.new(slack_url)
+    message = "今日のタスクをやりましたか？ #{Rails.application.routes.url_helpers.progress_bar_url(self)}"
+    notifier.ping message
   end
 end
--- lib/tasks/slack_notify.rake
+++ lib/tasks/slack_notify.rake
@@ -1,10 +1,6 @@
 namespace :batch do
-  desc 'Send slack notif='
+  desc 'Send slack notify'
   task :send_slack_notify do 
-    ProgressBar.where.not(slack_url: nil).where(notify_time: Time.current.to_s(:time)).each do |progress_bar|
-      notifier = Slack::Notifier.new(progress_bar.slack_url)
-      message = "今日のタスクをやりましたか？ #{progress_bar_url(progress_bar)}"
-      notifier.ping message
-    end
+    ProgressBar.notify_for(Time.current).map(&:send_slack_notify)
   end
 end
